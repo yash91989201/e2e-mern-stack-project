4bd705d5be44a397378342bd879040ced4438665
yash91989201 <yashraj6370@gmail.com>
2024-09-30 18:46:19 +0530

    update backend key

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    key            = "LockID"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
